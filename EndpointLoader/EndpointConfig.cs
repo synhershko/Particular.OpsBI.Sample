1e4e508451fae711f538f5b37a289699e67c34f6
Itamar Syn-Hershko <itamar@code972.com>
2014-09-04 19:36:10 +0300

    Cleanup and fix endpoint shutdown

--- EndpointLoader/EndpointConfig.cs
+++ EndpointLoader/EndpointConfig.cs
@@ -1,4 +1,6 @@
 
+using System.Globalization;
+
 namespace EndpointLoader
 {
     using NServiceBus;
@@ -16,52 +18,52 @@ namespace EndpointLoader
     {
         public IBus Bus { get; set; }
 
+        private int orderNumber;
+        private volatile bool shouldStop;
+        readonly Stopwatch sw = new Stopwatch();
+        private Thread thread;
+
         public void Start()
         {
-            System.Console.WriteLine("Starting");
+            Console.WriteLine("Starting");
+            Console.WriteLine("Click Enter to start sending");
+            Console.ReadLine();
+            Console.WriteLine("Sending");
 
-            int orderNumber = 0;
-
-            System.Console.WriteLine("Click Enter to start sending");
-            System.Console.ReadLine();
-            System.Console.WriteLine("Sending");
-
-
-            Stopwatch sw = new Stopwatch();
-
-            LoopSendMessage(orderNumber, sw);
-        
+            shouldStop = false;
+            thread = new Thread(LoopSendMessage);
+            thread.Start();
         }
 
-        private void LoopSendMessage(int orderNumber, Stopwatch sw)
+        private void LoopSendMessage()
         {
-            while (true)
+            while (!shouldStop)
             {                
                 sw.Start();
                 
-                orderNumber = SendMessage(orderNumber);
+                var num = SendMessage();
 
                 // On peak we sleep less and send more
                 var rand = SampleRandomization.IsPeak() ? 100 : 1000;
                 Thread.Sleep(SampleRandomization.Random.Next(rand));
 
-                if (orderNumber % 1000 == 0)
+                if (num % 1000 == 0)
                 {
                     sw.Stop();
                     var msgPerSec = (1000000 / sw.ElapsedMilliseconds);
-                    System.Console.WriteLine("{0} msg/sec", msgPerSec);
+                    Console.WriteLine("{0} msg/sec", msgPerSec);
                     sw.Reset();
                 }
             }
         }
 
-        private int SendMessage(int orderNumber)
+        private int SendMessage()
         {
             var command = new SubmitOrder
             {
-                ClientId = Thread.CurrentThread.ManagedThreadId.ToString(),
+                ClientId = Thread.CurrentThread.ManagedThreadId.ToString(CultureInfo.InvariantCulture),
                 OrderNumber = Interlocked.Increment(ref orderNumber),
-                VideoIds = new string[] { "intro2", "intro1"},
+                VideoIds = new[] { "intro2", "intro1"},
                 EncryptedCreditCardNumber = "GwPEFVxH9Sc53E7/IuvTlCdPVnS54f/vtFWFYdC/+Fs=@8qP13Poio7oXF5lDlDbyGQ==",
                 EncryptedExpirationDate = "EVNMPEVijulYdA9Dz2sJNw==@yd1497lzzIykLg2FEmTqkQ=="
             };
@@ -72,8 +74,9 @@ namespace EndpointLoader
 
         public void Stop()
         {
-            System.Console.WriteLine("Stopping");
+            shouldStop = true;
+            Console.WriteLine("Stopping");
+            thread.Join();
         }
-
     }
 }
